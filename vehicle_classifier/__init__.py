93d50c56a8712bcc0db99180154a32acbd3f1197
sirirouser <yu.q.lin12@gmail.com>
2026-08-08 17:16:39 +0000

    Remove vehicle classifier

--- vehicle_classifier/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Public package API for vehicle attribute checkpoint inference."""
-
-from .predictor import VehicleClassifier, vehicle_classifier
-
-__all__ = ["VehicleClassifier", "vehicle_classifier"]
